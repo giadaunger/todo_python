42cf39eec7d37bcac6b068db52a0461988ebe746
Giada Unger <giada.unger02@gmail.com>
2023-11-19 09:42:43 +0100

    Added a menu and a user input

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from re import M
-from tkinter import Menu
-
-
 class TodoDatabase:
     def __init__(self) -> None:
         self.todo_list = []
@@ -10,6 +6,21 @@ class TodoDatabase:
 class TodoMenu():
     def __init__(self) -> None:
         todo_db = TodoDatabase()
+        self.start_menu()
+
+    def start_menu(self):
+        menu_options = [
+            "[1] Show todos",
+            "[2] Add todo",
+            "[3] Delete todo / Mark a todo as done",
+            "[4] Edit a todo"
+        ]
+
+        while True:
+            for menu_option in menu_options:
+                print(menu_option)
+            
+            user_menu_input = input("\nWhat would you like to do? ")
 
 
 if __name__ == "__main__":
