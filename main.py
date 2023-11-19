a23354a3a2e702e94292a7e53160fd0ec03c30dd
Giada Unger <giada.unger02@gmail.com>
2023-11-19 10:23:26 +0100

    Created a function to load all the todos from the todos.json file (tet)

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import json
+
 class TodoDatabase:
     def __init__(self) -> None:
         self.todo_list = []
+        self.load_todos()
+
+    def load_todos(self):
+        with open("todos.json", "r") as f:
+            self.todo_list = json.load(f)
+            return self.todo_list
 
 
 class TodoMenu():
     def __init__(self) -> None:
-        todo_db = TodoDatabase()
+        self.todo_db = TodoDatabase()
         self.start_menu()
 
     def start_menu(self):
         menu_options = [
-            "[1] Show todos",
+            "[1] Show all todos",
             "[2] Add todo",
             "[3] Delete todo / Mark a todo as done",
             "[4] Edit a todo",
@@ -25,13 +33,13 @@ class TodoMenu():
 
             if user_menu_input == "Q" or user_menu_input == "q":
                 pass
-            if user_menu_input == "1":
+            elif user_menu_input == "1":
                 pass
-            if user_menu_input == "2":
+            elif user_menu_input == "2":
                 pass
-            if user_menu_input == "3":
+            elif user_menu_input == "3":
                 pass
-            if user_menu_input == "4":
+            elif user_menu_input == "4":
                 pass
             else:
                 print("No such option available, please try again!\n")
