0cf3cce3c55c917d6da38a8278a4e1455f2b492d
Giada Unger <giada.unger02@gmail.com>
2023-11-19 09:30:31 +0100

    Added a second class to represent the menu/frontend and initialited the todo_db in the TodoMenu() initializer

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from re import M
+from tkinter import Menu
+
+
 class TodoDatabase:
     def __init__(self) -> None:
         self.todo_list = []
 
 
-todo_db = TodoDatabase()
+class TodoMenu():
+    def __init__(self) -> None:
+        todo_db = TodoDatabase()
+
+
+if __name__ == "__main__":
+    menu = TodoMenu()
