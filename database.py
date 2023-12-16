b579e85db0aa2793aee078d1d1cad83f0e7c486c
Giada Unger <giada.unger02@gmail.com>
2023-12-16 11:14:42 +0100

    Created a function that creates the tables needed and executes them

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_PORT = os.getenv("DATABASE_PORT") 
 DATABASE_USER = os.getenv("DATABAATABASE_NAMSE_USER") 
 
+
 def connect_db():
     """Establishes a connection to the database."""
     try:
@@ -18,3 +19,25 @@ def connect_db():
     except psycopg2.DatabaseError as e:
         print(f"Database connection failed: {e}")
         raise
+
+
+def create_tables(con):
+    create_table_category = """
+    CREATE TABLE IF NOT EXISTS categories(
+        id SERIAL PRIMARY KEY,
+        category_name VARCHAR(100) UNIQUE NOT NULL
+    )
+    """
+
+    create_table_todo = """
+    CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        todo_name VARCHAR(100) NOT NULL,
+        category_id INT REFERENCES categories(id)
+    )
+    """
+
+    with con:
+        with con.cursor() as cursor:
+            cursor.execute(create_table_category)
+            cursor.execute(create_table_todo)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import database as db
+import psycopg2
+
+# Main execution logic
+def main(con):
+    """
+    Main menu
+    """
+    pass
+
 
 if __name__ == '__main__':
-    con = db.connect_db
+    con = db.connect_db()
+    db.create_tables(con=con)
+    main(con=con)
