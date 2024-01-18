16e0e9d39730f005e9e1743f8959176213bd5630
Giada Unger <giada.unger02@gmail.com>
2024-01-18 12:25:08 +0100

    Made GET requests for fetching all of the todos and categories

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ import database as db
 app = Flask(__name__)
 load_dotenv()
 con = db.connect_db()
+
+@app.route("/todos", methods=["GET"])
+def get_all_todos():
+    todos = db.get_todos(con)
+    if not todos:
+        return {"message": "No todos found"}, 404
+    return todos, 200
+
+@app.route("/categories", methods=["GET"])
+def get_all_categories():
+    categories = db.get_categories(con)
+    if not categories:
+        return {"message": "No categories found"}, 404
+    return categories, 200
